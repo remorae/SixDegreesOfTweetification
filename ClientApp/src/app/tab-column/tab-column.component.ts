aa13542c39a8d98878d54cfe88c4836f305165ce
Konnor Welsch <kwelsch@2ndwatch.com>
2018-02-05 22:23:33 -0800

    Add colors to variables and put some very basic layout in UI

---
+++ ClientApp/src/app/tab-column/tab-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-column',
+  templateUrl: './tab-column.component.html',
+  styleUrls: ['./tab-column.component.scss']
+})
+export class TabColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
