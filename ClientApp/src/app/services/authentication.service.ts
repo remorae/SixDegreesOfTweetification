41f08d7f1fa8ac7e8cddf61abd79837e0e5c97f3
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-02-06 19:04:06 -0800

    change login comp to use input username and password, create endpoint  to hit for login, flesh out AuthenticationService that tracks whether user has logged in.

--- ClientApp/src/app/services/authentication.service.ts
+++ ClientApp/src/app/services/authentication.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { CanActivate } from '@angular/router/src/interfaces';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/Observable/of';
@@ -7,17 +7,35 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/operators/map';
 
+export interface Creds {
+    username: string;
+    password: string;
+}
 @Injectable()
 export class AuthenticationService {
-    constructor() {}
+    constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
 
     isLoggedIn = false;
     redirectUrl: string;
-    login(): Observable<boolean> {
-        // this does nothing but make the user wait and log them in.
-        return Observable.of(true)
-            .delay(1000)
-            .do(val => (this.isLoggedIn = true));
+    private baseUrl: string;
+    login(user: string, pass: string): Observable<boolean> {
+        const creds: Creds = {
+            username: user,
+            password: pass
+        };
+        return this.http
+            .post<boolean>(this.baseUrl + 'api/Authentication/Login', creds, {
+                headers: new HttpHeaders({
+                    'Content-Type': 'application/json; charset=utf-8'
+                })
+            })
+            .do(val => {
+                this.isLoggedIn = val;
+            });
     }
 }
