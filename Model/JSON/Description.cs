d5eef77c8d62d7857d9d3edd17a93def75779f98
Alex Plagman <alexplagman@gmail.com>
2018-02-07 17:11:19 -0800

    Add TweetSearchController

---
+++ Model/JSON/Description.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SixDegrees.Model.JSON
+{
+    public partial class Description
+    {
+        [JsonProperty("urls")]
+        public List<Url> Urls { get; set; }
+    }
+}
---
+++ Model/JSON/VideoInfo.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SixDegrees.Model.JSON
+{
+    public partial class VideoInfo
+    {
+        [JsonProperty("aspect_ratio")]
+        public List<long> AspectRatio { get; set; }
+
+        [JsonProperty("variants")]
+        public List<Variant> Variants { get; set; }
+
+        [JsonProperty("duration_millis")]
+        public long? DurationMillis { get; set; }
+    }
+}
