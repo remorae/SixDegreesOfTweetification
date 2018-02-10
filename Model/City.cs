4999ce92e58f811ff08fcb420e2c1965d6f4b8ac
Alex Plagman <alexplagman@gmail.com>
2018-02-10 10:21:04 -0800

    Create data model / API endpoint skeleton for hashtag location data
    Encapsulate common request logic

---
+++ Model/City.cs
@@ -0,0 +1,14 @@
+﻿namespace SixDegrees.Model
+{
+    public class City
+    {
+        public string CityName { get; }
+        public string[] Hashtags { get; }
+
+        public City(string cityName, string[] hashtags)
+        {
+            CityName = cityName;
+            Hashtags = hashtags;
+        }
+    }
+}
---
+++ Model/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace SixDegrees.Model
+{
+    public class Country
+    {
+        public string CountryName { get; }
+        public City[] Cities { get; }
+
+        public Country(string countryName, City[] cities)
+        {
+            CountryName = countryName;
+            Cities = cities;
+        }
+    }
+}
