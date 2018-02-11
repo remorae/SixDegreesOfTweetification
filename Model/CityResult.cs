4441ada6ccb5498aa2045a597f963923fd776699
Mori <me@morine.net>
2018-02-11 15:53:33 -0800

    Return cities as normal array, encapsulate logic

---
+++ Model/CityResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SixDegrees.Model
+{
+    public class CityResult
+    {
+        public string CityName { get; }
+        public IEnumerable<string> Hashtags { get; }
+
+        public CityResult(string cityName, IEnumerable<string> hashtags)
+        {
+            CityName = cityName;
+            Hashtags = hashtags;
+        }
+    }
+}
