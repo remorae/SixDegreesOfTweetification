64aac9df549c9ed8b72d5978056a6b291db8259c
Konnor Welsch <kwelsch@2ndwatch.com>
2018-02-14 22:51:29 -0800

    Create Geo page; Create Country & Place model

---
+++ ClientApp/src/app/geo-page/geo-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GeoPageComponent } from './geo-page.component';
+
+describe('GeoPageComponent', () => {
+  let component: GeoPageComponent;
+  let fixture: ComponentFixture<GeoPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GeoPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GeoPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
