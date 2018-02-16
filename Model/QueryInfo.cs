724a2bdecfa829eb3fac0be635bac3dec121963a
Alex Plagman <alexplagman@gmail.com>
2018-02-15 19:27:15 -0800

    Store query info per endpoint

---
+++ Model/QueryInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace SixDegrees.Model
+{
+    public struct QueryInfo
+    {
+        public string LastQuery;
+        public string LastMaxID;
+
+        public QueryInfo(string lastQuery, string lastMaxID)
+        {
+            LastQuery = lastQuery;
+            LastMaxID = lastMaxID;
+        }
+    }
+}
