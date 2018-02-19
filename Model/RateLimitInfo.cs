b288ea245d009a24739f7de3e09832731b7242fc
Alex Plagman <alexplagman@gmail.com>
2018-02-18 18:38:14 -0800

    Add default rate limit info

--- Model/RateLimitInfo.cs
+++ Model/RateLimitInfo.cs
@@ -4,7 +4,7 @@ namespace SixDegrees.Model
 {
     public class RateLimitInfo
     {
-
+        //TODO: Get and store limits by Twitter API endpoint to support multiple-endpoint operations
         public static bool SupportsAppAuth(QueryType type)
         {
             switch (type)
@@ -23,40 +23,32 @@ namespace SixDegrees.Model
             }
         }
 
-        public static int AppAuthLimit(QueryType type)
+        public static (int User, int Application) AuthLimits(QueryType type)
         {
-            if (!SupportsAppAuth(type))
-                return 0;
-
             switch (type)
             {
+                case QueryType.TweetsByHashtag:
+                case QueryType.LocationsByHashtag:
+                    return (180, 450);
+                case QueryType.UserByScreenName:
+                    return (900, 900);
+                case QueryType.UserConnectionsByScreenName:
+                    return (900, 300);
                 default:
-                    return 0;
+                    return (0, 0);
             }
         }
 
-        public static int UserAuthLimit(QueryType type)
-        {
-            if (!SupportsUserAuth(type))
-                return 0;
-
-            switch (type)
-            {
-                default:
-                    return 0;
-            }
-        }
-
-        private DateTime? lastUpdated = null;
+        private DateTime lastUpdated = DateTime.Now;
 
         public int AppAuthRemaining { get; private set; }
         public int UserAuthRemaining { get; private set; }
-        public TimeSpan SinceLastUpdate => (lastUpdated.HasValue) ? DateTime.Now - lastUpdated.Value : TimeSpan.MaxValue;
+        public TimeSpan SinceLastUpdate => DateTime.Now - lastUpdated;
 
         public RateLimitInfo(QueryType type)
         {
-            AppAuthRemaining = AppAuthLimit(type);
-            UserAuthRemaining = UserAuthLimit(type);
+            AppAuthRemaining = AuthLimits(type).Application;
+            UserAuthRemaining = AuthLimits(type).User;
         }
 
         public void Update(int appAuthRemaining, int userAuthRemaining)
