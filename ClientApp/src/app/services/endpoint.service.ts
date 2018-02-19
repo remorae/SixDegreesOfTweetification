e6c2a7e7a9eb9d0a136ac1db4617ef072d2cec14
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-02-19 09:41:41 -0800

    Add Subject for Rate Limit data from endpoint

--- ClientApp/src/app/services/endpoint.service.ts
+++ ClientApp/src/app/services/endpoint.service.ts
@@ -2,10 +2,17 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Country } from '../models';
+import { Subject } from 'rxjs/Subject';
 
+export type QueryType =
+    |'TweetsByHashtag'
+    | 'LocationsByHashtag'
+    | 'UserByScreenName'
+    | 'UserConnectionsByScreenName';
 @Injectable()
 export class EndpointService {
     baseUrl: string;
+    latestEndpoint: Subject<number[]> = new Subject<number[]>();
     constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
         this.baseUrl = baseUrl;
     }
@@ -21,5 +28,11 @@ export class EndpointService {
         return this.http.get<Country[]>(
             this.baseUrl + 'api/search/locations?query=' + hashtag
         );
+    }
+
+    public pushLatest(type: QueryType) {
+        this.http
+            .get<number[]>(this.baseUrl + 'api/ratelimit/status?=' + type)
+            .subscribe(val => this.latestEndpoint.next(val));
     }
 }
