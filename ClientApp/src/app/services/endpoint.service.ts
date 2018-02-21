50edff3331e234ffb7812abc00617382e579949a
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-02-20 19:35:29 -0800

    Add rate limit display component, Add callback to tweet data endpoints

--- ClientApp/src/app/services/endpoint.service.ts
+++ ClientApp/src/app/services/endpoint.service.ts
@@ -4,35 +4,59 @@ import { Observable } from 'rxjs/Observable';
 import { Country } from '../models';
 import { Subject } from 'rxjs/Subject';
 
-export type QueryType =
-    |'TweetsByHashtag'
-    | 'LocationsByHashtag'
-    | 'UserByScreenName'
-    | 'UserConnectionsByScreenName';
+export enum QueryType {
+    'TweetsByHashtag',
+    'LocationsByHashtag',
+    'UserByScreenName',
+    'UserConnectionsByScreenName'
+}
+export class AuthPair {
+    Application: number;
+    User: number;
+}
+export class RateInfo {
+    TweetsByHashtag: AuthPair;
+    LocationsByHashtag: AuthPair;
+    UserByScreenName: AuthPair;
+    UserConnectionsByScreenName: AuthPair;
+}
+
 @Injectable()
 export class EndpointService {
     baseUrl: string;
-    latestEndpoint: Subject<number[]> = new Subject<number[]>();
+    private hitBackend: Subject<any> = new Subject<any>();
+    pushLatest = () => {
+        this.hitBackend.next();
+    };
+
     constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
         this.baseUrl = baseUrl;
     }
 
+    public watchEndPoints() {
+        return this.hitBackend;
+    }
+    public getAllRateLimits() {
+        return this.http.get<RateInfo>(this.baseUrl + 'api/ratelimit/all');
+    }
     public searchTweets(hashtag: string): Observable<any[]> {
         // TODO: Change type to properly match final Endpoint Version
-        return this.http.get<any[]>(
-            this.baseUrl + 'api/search/tweets?query=' + hashtag
-        );
+        return this.http
+            .get<any[]>(this.baseUrl + 'api/search/tweets?query=' + hashtag)
+            .do(this.pushLatest);
     }
 
     public searchLocations(hashtag: string): Observable<Country[]> {
-        return this.http.get<Country[]>(
-            this.baseUrl + 'api/search/locations?query=' + hashtag
-        );
+        return this.http
+            .get<Country[]>(
+                this.baseUrl + 'api/search/locations?query=' + hashtag
+            )
+            .do(this.pushLatest);
     }
 
-    public pushLatest(type: QueryType) {
-        this.http
-            .get<number[]>(this.baseUrl + 'api/ratelimit/status?=' + type)
-            .subscribe(val => this.latestEndpoint.next(val));
+    public getSelectedRateLimit(type: QueryType) {
+        return this.http.get<AuthPair>(
+            this.baseUrl + 'api/ratelimit/status?=' + type + '&forceupdate=true'
+        );
     }
 }
