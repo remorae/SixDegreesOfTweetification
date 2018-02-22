c00fc4e8ae644ebe4aca341bf29a5f97930f9b1a
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-02-22 15:51:07 -0800

    change QueryType to string enum, add subs to RateLimitComp

--- ClientApp/src/app/rate-limit-display/rate-limit-display.component.ts
+++ ClientApp/src/app/rate-limit-display/rate-limit-display.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Query } from '@angular/core';
 import {
     EndpointService,
     QueryType,
-    RateInfo
+    RateInfo,
+    AuthPair
 } from '../services/endpoint.service';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'app-rate-limit-display',
@@ -11,19 +15,64 @@ import {
     styleUrls: ['./rate-limit-display.component.scss']
 })
 export class RateLimitDisplayComponent implements OnInit {
-    rates: RateInfo;
-
-    constructor(private endpoint: EndpointService) {}
-
+    allRates: RateInfo;
+    currentComponentName: string;
+    currentPagesRates: any[];
+    constructor(private endpoint: EndpointService, private router: Router) {}
     ngOnInit() {
+        this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .map((event: NavigationEnd) =>
+                event.urlAfterRedirects.split('/').join('')
+            )
+            .subscribe((componentName: string) => {
+                this.currentComponentName = componentName;
+                this.requestRelatedRates(componentName);
+            });
+
+        this.endpoint.getAllRateLimits().subscribe(rates => {
+            this.translateRates(rates);
+        });
+
         this.endpoint.watchEndPoints().subscribe(() => {
             this.endpoint.getAllRateLimits().subscribe(rates => {
                 this.translateRates(rates);
             });
+            this.requestRelatedRates(this.currentComponentName);
         });
     }
 
+    public mapComponentToQueryType(componentName: string): [QueryType] {
+        switch (componentName) {
+            case 'geo':
+                return [QueryType.LocationsByHashtag];
+
+            default:
+                return null;
+        }
+    }
+
+    public requestRelatedRates(componentName: string) {
+        const types: QueryType[] = this.mapComponentToQueryType(componentName);
+
+        if (types) {
+            this.currentPagesRates = types.map(type => {
+                const rateName = type.replace(/([A-Z])/g, ' $1').substring(1);
+                this.endpoint
+                    .getSelectedRateLimit(type)
+                    .subscribe((auths: AuthPair) => {
+                        this.currentPagesRates.find(
+                            element => element[rateName]
+                        )[rateName] = auths;
+                    });
+
+                return { [rateName]: 'placeholder' };
+            });
+        } else {
+        }
+    }
+
     public translateRates(rates: RateInfo) {
-        this.rates = rates;
+        this.allRates = rates;
     }
 }
