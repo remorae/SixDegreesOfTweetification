b24a9b95bdea9f82c8cf4449d48796318f78a4c3
Konnor Welsch <kwelsch@2ndwatch.com>
2018-02-24 17:50:37 -0800

    Remove default pages

--- ClientApp/src/app/tile-column/tile-column.component.ts
+++ ClientApp/src/app/tile-column/tile-column.component.ts
@@ -20,16 +20,6 @@ export class TileColumnComponent implements OnInit {
             description: 'The place where the deer and the antelope play. Maybe Nebraska? Hard to say.',
         },
         {
-            title: 'Counter',
-            route: 'counter',
-            description: 'Default component that we kept so our app looks less boring. Check it out!',
-        },
-        {
-            title: 'Fetch Data',
-            route: 'fetch-data',
-            description: 'I\'m not entirely sure where the data comes from, but it\'s pretty magical.',
-        },
-        {
             title: 'Geographical Stats',
             route: 'geo',
             description: 'See where in the world certain hashtags are coming from!',
