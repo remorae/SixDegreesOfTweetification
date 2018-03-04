a42bb72c3bb69469c0c19b2d6319e452eb3cc566
Alex Plagman <alexplagman@gmail.com>
2018-03-03 19:29:50 -0800

    Use Identity for user auth

---
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace SixDegrees.Extensions
+{
+    static class ClaimsPrincipalExtensions
+    {
+        internal static string GetTwitterAccessToken(this ClaimsPrincipal user)
+            => (user.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(claim => claim.Type == Startup.AccessTokenClaim)?.Value;
+        internal static string GetTwitterAccessTokenSecret(this ClaimsPrincipal user)
+            => (user.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(claim => claim.Type == Startup.AccessTokenSecretClaim)?.Value;
+    }
+}
