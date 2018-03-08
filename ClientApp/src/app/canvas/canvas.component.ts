8aa9a0b54e02ce2859027ed54878c16c9a664ea1
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-03-08 12:25:05 -0800

    Add Citation

--- ClientApp/src/app/canvas/canvas.component.ts
+++ ClientApp/src/app/canvas/canvas.component.ts
@@ -22,7 +22,7 @@ export class CanvasComponent implements OnInit {
     }
 
 
-    example() {
+    example() { // https://github.com/jasondavies/d3-cloud
         const words = ['Hello', 'world', 'normally', 'you', 'want', 'more', 'words', 'than', 'this']
             .map(function (d) {
                 return { text: d, size: 10 + Math.random() * 90 };
