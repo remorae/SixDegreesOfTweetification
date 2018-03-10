d33e79568355c67a1f3cf593ca7977d1917a7c12
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-03-10 10:20:03 -0800

    Hook up cloudbottle to API,  Redraw cloud on fresh data

--- ClientApp/src/app/cloud-bottle/cloud-bottle.component.ts
+++ ClientApp/src/app/cloud-bottle/cloud-bottle.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as D3 from 'd3';
+import * as cloud from 'd3-cloud';
 import { CloudDataService } from '../services/cloud-data.service';
 declare let d3: any;
 
@@ -15,39 +16,52 @@ export interface WeightedWord {
     templateUrl: './cloud-bottle.component.html',
     styleUrls: ['./cloud-bottle.component.scss']
 })
-export class CloudBottleComponent implements OnInit {
+export class CloudBottleComponent implements OnInit, OnChanges {
 
-    words: WeightedWord[];
+    @Input() words: WeightedWord[];
     cloudWidth: string;
     cloudHeight: string;
-    constructor(private cloudData: CloudDataService) { }
+    differ;
+    layout;
+    constructor() { }
 
     ngOnInit() {
         this.cloudHeight = '600';
         this.cloudWidth = '940';
-        this.words = this.cloudData.init();
-        this.buildLayout();
+    }
+    ngOnChanges(changes: SimpleChanges) {
+        const wordChange = changes['words'];
+        if (wordChange.previousValue && wordChange.previousValue.length !== wordChange.currentValue.length) {
+            this.dropCloud();
+            this.buildLayout();
+        }
     }
 
     buildLayout() {
-
         d3.layout.cloud().size([this.cloudWidth, this.cloudHeight])
             .words(this.words)
             .padding(1)
-            .rotate(function () { return ~~(Math.random() * 2) * 45; })
+            .rotate(() => ~~(Math.random() * 2) * 45)
             // turns out ~~ just chops off everything to the right of the decimal
             .font('Impact')
-            .fontSize(function (d) { return d.size; })
-            .on('end', (input) => { // doesn't work without this arrow function, dunno why
+            .fontSize((d) => d.size)
+            .on('end', (input) => { // doesn't work without this arrow function
                 this.createCloud(input);
 
             })
             .start();
     }
+
+    dropCloud() {
+        D3.select('svg.removable').remove();
+        //  map is necessary to avoid subsequent calls messing with the attached sprite masks
+        this.words = this.words.map((word) => ({ text: word.text, size: word.size }));
+    }
     createCloud(input) {
         const fill: D3.ScaleOrdinal<string, string> = D3.scaleOrdinal(D3.schemeCategory10);
         D3.select('.bottle')
             .append('svg')
+            .attr('class', 'removable')
             .attr('width', this.cloudWidth)
             .attr('height', this.cloudHeight)
             .append('g')
@@ -62,51 +76,6 @@ export class CloudBottleComponent implements OnInit {
             .attr('transform', (d: any) => // changed type to any
                 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')'
             )
-            .text((d: WeightedWord) =>  d.text);
+            .text((d: WeightedWord) => d.text);
     }
-    example() { // https://github.com/jasondavies/d3-cloud
-
-        //let blues : D3.ScaleOrdinal<string, string> = D3.scaleOrdinal(D3.schemeBlues.);
-        //  D3.schemeBlues
-        //  D3.schemeCategory10
-
-        let fill: D3.ScaleOrdinal<string, string> = D3.scaleOrdinal(D3.schemeCategory10);
-        d3.layout.cloud().size([600, 600])
-            // .canvas(() => this.canvas.nativeElement)
-            .words(this.words)
-            .padding(5)
-            .rotate(function () { return ~~(Math.random() * 2) * 45; })
-            // turns out ~~ just chops off everything to the right of the decimal
-            .font('Impact')
-            .fontSize(function (d) { return d.size; })
-            .on('end', (input) => {
-                // console.log(JSON.stringify(words));
-                D3.select('.bottle')
-                    .append('svg')
-                    .attr('width', 600)
-                    .attr('height', 600)
-                    .append('g')
-                    .attr('transform', 'translate(' + 600 / 2 + ',' + 600 / 2 + ')')
-                    .selectAll('text')
-                    .data(this.words)
-                    .enter().append('text')
-                    .style('font-size', function (d: WeightedWord) { return d.size + 'px'; })
-                    .style('font-family', 'Impact')
-                    .style('fill', function (d, i) { return fill(i.toString()); }) // changed 'i' to 'i.toString()'
-                    .attr('text-anchor', 'middle')
-                    .attr('transform', function (d: any) { //changed type to any
-                        return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
-                    })
-                    .text(function (d: WeightedWord) { return d.text; });
-            })
-            .start();
-
-
-
-
-
-
-    }
-
-
 }
--- ClientApp/src/app/word-cloud-page/word-cloud-page.component.ts
+++ ClientApp/src/app/word-cloud-page/word-cloud-page.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { UserInput } from '../models/userInput';
-
+import { CloudDataService } from '../services/cloud-data.service';
+import { WeightedWord } from '../cloud-bottle/cloud-bottle.component';
+import * as D3 from 'd3';
 @Component({
-  selector: 'app-word-cloud-page',
-  templateUrl: './word-cloud-page.component.html',
-  styleUrls: ['./word-cloud-page.component.scss']
+    selector: 'app-word-cloud-page',
+    templateUrl: './word-cloud-page.component.html',
+    styleUrls: ['./word-cloud-page.component.scss']
 })
 export class WordCloudPageComponent implements OnInit {
     latestSearch: string;
-    constructor() { }
+    wordMap: D3.Map<WeightedWord> = D3.map();
+    cloudWords: WeightedWord[] = [];
+    newlyAdded: number;
+    constructor(private cloudData: CloudDataService) { }
 
-    ngOnInit() {}
+    ngOnInit() {
+    }
     onUserSubmit(input: UserInput) {
         this.latestSearch = input.inputs[0];
+        this.cloudData.getRelatedHashes(this.latestSearch).subscribe((newWords: WeightedWord[]) => {
+            newWords.forEach((word) => {
+                if (this.wordMap.has(word.text)) {
+                    this.wordMap.get(word.text).size++;
+                } else {
+                    this.wordMap.set(word.text, word);
+                }
+            });
+            const fresh: WeightedWord[] = this.wordMap.values();
+            this.newlyAdded = fresh.length - this.cloudWords.length;
+            if (!this.cloudWords.length) {
+                this.newlyAdded = newWords.length;
+            }
+            this.cloudWords = this.wordMap.values();
+        });
+
     }
 }
