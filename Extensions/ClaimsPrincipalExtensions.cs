b386f8d59d195f5cc328c39dc58bacb38b9410a5
Alex Plagman <alexplagman@gmail.com>
2018-03-15 19:20:11 -0700

    Store user rate limit cache in a database, per user!

--- Extensions/ClaimsPrincipalExtensions.cs
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SixDegrees.Model;
 
 namespace SixDegrees.Extensions
 {
@@ -9,5 +12,10 @@ namespace SixDegrees.Extensions
             => (user.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(claim => claim.Type == Startup.AccessTokenClaim)?.Value;
         internal static string GetTwitterAccessTokenSecret(this ClaimsPrincipal user)
             => (user.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(claim => claim.Type == Startup.AccessTokenSecretClaim)?.Value;
+
+        internal static async Task<ApplicationUser> GetCurrentApplicationUser(this UserManager<ApplicationUser> manager, ClaimsPrincipal user)
+        {
+            return (user.Identity.Name != null) ? await manager.FindByNameAsync(user.Identity.Name) : null;
+        }
     }
 }
