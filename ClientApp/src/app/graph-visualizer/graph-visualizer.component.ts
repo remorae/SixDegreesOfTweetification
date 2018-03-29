1581ba16699d0862c5c9f10a1e7043c8ae107a74
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-03-29 14:29:49 -0600

    Refactor G-Visualizer; Hook up data-service to Endpoint

--- ClientApp/src/app/graph-visualizer/graph-visualizer.component.ts
+++ ClientApp/src/app/graph-visualizer/graph-visualizer.component.ts
@@ -1,83 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import * as D3 from 'd3';
-import { GraphDataService } from '../services/graph-data.service';
+import { Node, Link, Graph, GraphDataService } from '../services/graph-data.service';
+import { ForceLink } from 'd3';
 @Component({
     selector: 'app-graph-visualizer',
     templateUrl: './graph-visualizer.component.html',
     styleUrls: ['./graph-visualizer.component.scss']
 })
-export class GraphVisualizerComponent implements OnInit {
+export class GraphVisualizerComponent implements OnInit, OnChanges {
 
-    constructor(private graphData: GraphDataService) {
+    @Input() graph: { links, nodes };
+    readonly svgHeight = 900;
+    readonly svgWidth = 900;
+    readonly maxForceDistance = 250;
+    constructor() {
 
     }
 
     ngOnInit() {
 
-        this.drawGraph();
+
     }
 
-    drawGraph() {
-        const maxForceDistance = 250;
-        const svg = D3.select('svg.target'),
-            width = 900, // +svg.attr('width'),
-            height = 900; // +svg.attr('height');
+    ngOnChanges(changes: SimpleChanges) {
+        const graphChange = changes['graph'];
+        if (graphChange.currentValue) {
+            this.drawGraph();
+           // this.deleteGraph();
+        }
+    }
 
-        const color = D3.scaleOrdinal(D3.schemeCategory10);
+    deleteGraph() {
+            D3.select('svg.graph-container').selectAll('*').remove();
+    }
 
-        const simulation: any = D3.forceSimulation()  // may need to remove
+    createSimulation(): D3.Simulation<Node, Link> {
+
+        return D3.forceSimulation<Node, Link>()
             .force('link', D3.forceLink().id((d: { id }) => d.id))
-            .force('charge', D3.forceManyBody().distanceMax(maxForceDistance))
+            .force('charge', D3.forceManyBody().distanceMax(this.maxForceDistance))
+            .force('center', D3.forceCenter(this.svgWidth / 2, this.svgHeight / 2));
+    }
 
-            .force('center', D3.forceCenter(width / 2, height / 2));
-        let graph: { links, nodes } = this.graphData.mapUserDataToLinks(); // this probably needs to go back to D3.json
-        let link = svg.append('g')
-            //  .attr('class', 'links')
+    createLinksGroup(svg: D3.Selection<D3.BaseType, {}, HTMLElement, any>): D3.Selection<D3.BaseType, {}, D3.BaseType, {}> {
+        return svg.append('g')
+            .attr('class', 'links')
             .attr('stroke', '#999')
             .attr('stroke-opacity', '0.6')
             .selectAll('line')
-            .data(graph.links)
+            .data(this.graph.links)
             .enter().append('line')
-            .attr('stroke-width', (d: { value }) => Math.sqrt(d.value));
+            .attr('stroke-width', (d: Link) => Math.sqrt(d.value));
+    }
 
-        let node = svg.append('g')
-            //   .attr('class', 'nodes')
+    createNodesGroup(svg: D3.Selection<D3.BaseType, {}, HTMLElement, any>, simulation: D3.Simulation<Node, Link>) {
+        const color = D3.scaleOrdinal(D3.schemeCategory10);
+        return svg.append('g')
+            .attr('class', 'nodes')
             .selectAll('circle')
-            .data(graph.nodes)
+            .data(this.graph.nodes)
             .enter().append('circle')
-            .attr('r', (d: { group }) => { return d.group !== 0 ? 5 : 10})
-            .attr('fill', (d: { group }) => color(d.group))
+            .attr('r', (d: Node) => d.group ? 5 : 10)
+            .attr('fill', (d: Node) => color(d.group.toString()))
             .call(D3.drag()
                 .on('start', dragstarted)
                 .on('drag', dragged)
                 .on('end', dragended));
 
-        node.append('title')
-            .text((d: { id }) => d.id);
-
-        simulation
-            .nodes(graph.nodes)
-            .on('tick', ticked);
-
-        simulation.force('link').links(graph.links);
-
-
-
-
-        function ticked() {
-            link
-                .attr('x1', (d: { source }) => d.source.x)
-                .attr('y1', (d: { source }) => d.source.y)
-                .attr('x2', (d: { target }) => d.target.x)
-                .attr('y2', (d: { target }) => d.target.y);
-
-            node
-                .attr('cx', (d: { x }) => d.x)
-                .attr('cy', (d: { y }) => d.y);
-        }
-
         function dragstarted(d) {
-            if (!D3.event.active) simulation.alphaTarget(0.3).restart();
+            if (!D3.event.active) {
+                simulation.alphaTarget(0.3).restart();
+            }
             d.fx = d.x;
             d.fy = d.y;
         }
@@ -88,10 +81,40 @@ export class GraphVisualizerComponent implements OnInit {
         }
 
         function dragended(d) {
-            if (!D3.event.active) simulation.alphaTarget(0);
+            if (!D3.event.active) {
+                simulation.alphaTarget(0);
+            }
             d.fx = null;
             d.fy = null;
         }
+    }
+    drawGraph() {
+        const svg = D3.select('svg.graph-container');
+        const simulation = this.createSimulation();
+        const links = this.createLinksGroup(svg);
+        const nodes = this.createNodesGroup(svg, simulation);
+
+        nodes.append('title')
+            .text((d: Node) => `User: ${d.id} Distance: ${d.group}`);
+
+        simulation
+            .nodes(this.graph.nodes)
+            .on('tick', ticked);
+
+        simulation.force<ForceLink<Node, Link>>('link').links(this.graph.links);
+
+        function ticked() {
+            links
+                .attr('x1', (d: { source }) => d.source.x)
+                .attr('y1', (d: { source }) => d.source.y)
+                .attr('x2', (d: { target }) => d.target.x)
+                .attr('y2', (d: { target }) => d.target.y);
+
+            nodes
+                .attr('cx', (d: { x }) => d.x)
+                .attr('cy', (d: { y }) => d.y);
+        }
+
 
     }
 
--- ClientApp/src/app/services/graph-data.service.ts
+++ ClientApp/src/app/services/graph-data.service.ts
@@ -1,30 +1,54 @@
 import { Injectable } from '@angular/core';
 import { EndpointService } from './endpoint.service';
 import { TestData } from './testdata';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { SimulationNodeDatum, SimulationLinkDatum } from 'd3';
 
-
-export interface Node {
+export interface Node extends SimulationNodeDatum {
     id: string;
     group: number;
 }
 
-export interface Link {
+export interface Link extends SimulationLinkDatum<Node> {
     source: string;
     target: string;
     value: number;
 }
 
+export interface Graph {
+    links: Link[];
+    nodes: Node[];
+}
+
 @Injectable()
 export class GraphDataService {
     testData: TestData = new TestData();
+    graphData: BehaviorSubject<Graph> = new BehaviorSubject<Graph>(null);
 
     constructor(private endpoint: EndpointService) {
     }
 
-    mapUserDataToLinks() {
-        const data = this.testData.getUserData();
+
+    getSingleUserData(username: string) {
+         this.graphData.next(this.mapUserData(this.testData.getUserData()));
+        // this.endpoint.searchUserDegrees(username)
+        //     .map(this.mapUserData)
+        //     .subscribe(
+        //         (graph: Graph) =>
+        //          this.graphData.next(graph),
+        //         (error) => {
+        //             console.log(error);
+        //         this.graphData.next(this.graphData.value);
+        //     });
+    }
+
+    getLatestGraphData() {
+        return this.graphData;
+    }
+    mapUserData = (data): Graph => {
+        // const data = this.testData.getUserData();
         const nodes: Node[] = Object.keys(data)
-                                .map((user) => ({ id: user, group: data[user].distance + 1 }));
+            .map((user) => ({ id: user, group: data[user].distance }));
 
         const links: Link[] = this.createLinks(data, nodes);
         return { nodes: nodes, links: links };
@@ -32,16 +56,14 @@ export class GraphDataService {
 
 
     createLinks(data, nodes: Node[]) {
-        let links: Link[] = [];
-        //const root: string = nodes.find((a)=> a.group === 0);
+        const links: Link[] = [];
 
         nodes.forEach(element => {
-            const connections = data[element.id].connections || [];
-
+            const user = data[element.id];
+            const connections = user ? user.connections : [];
             connections.forEach(connection => {
                 links.push({ source: element.id, target: connection.screenName, value: 1 })
             });
-
         });
 
         return links;
