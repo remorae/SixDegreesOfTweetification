6e1d595a445bd73af469d4aec70270a7e40d17d4
Konnor Welsch <kwelsch@2ndwatch.com>
2018-03-31 23:10:44 -0700

    Create section SVGs and put in Tiles column

--- ClientApp/src/app/tile-column/tile-column.component.ts
+++ ClientApp/src/app/tile-column/tile-column.component.ts
@@ -18,25 +18,25 @@ export class TileColumnComponent implements OnInit {
             title: 'Hash to Hash',
             route: 'hash-to-hash',
             description: 'How many tweets does it take to get from one hashtag to another?',
-            iconPath: './../../assets/Bandaid.svg'
+            iconPath: './../../assets/hash_to_hash.svg'
         },
         {
             title: 'Geo Stats',
             route: 'geo',
             description: 'See where in the world certain hashtags are coming from!',
-            iconPath: './../../assets/Bandaid.svg'
+            iconPath: './../../assets/geo_stats.svg'
         },
         {
             title: 'User to User',
             route: 'user-to-user',
             description: 'How many user connections does it take to get from one person to another?',
-            iconPath: './../../assets/Bandaid.svg'
+            iconPath: './../../assets/user_to_user.svg'
         },
         {
             title: 'Word Cloud',
             route: 'word-cloud',
             description: 'See what hashtags are often found together!',
-            iconPath: './../../assets/Bandaid.svg'
+            iconPath: './../../assets/word_cloud.svg'
         },
     ];
 
