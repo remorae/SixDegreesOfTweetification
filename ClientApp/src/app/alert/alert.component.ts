7b80174a5151accb49903c4a1d0b096122f9924c
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-04-18 18:14:10 -0700

    Update alertComponent CSS to be lower right

--- ClientApp/src/app/alert/alert.component.ts
+++ ClientApp/src/app/alert/alert.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Message {
+    header: string;
+    content: string;
+    style: string;
+    dismissed: boolean = false;
+
+    constructor(header,content, style?) {
+        this.header = header;
+        this.content = content;
+        this.style = style || 'info';
+    }
+
+}
+
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+    selector: 'app-alert',
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent implements OnInit {
 
-  constructor() { }
+    messages: Message[] = [];
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.messages.push(new Message('Error', 'The server responded with: 404 The given user does not exist','danger'));
+    }
 
+    dismiss(itemKey) {
+        //this.toast.dismissMessage(itemKey)
+    }
+    // https://angularfirebase.com/lessons/angular-toast-message-notifications-from-scratch/
 }
