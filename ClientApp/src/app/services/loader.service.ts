ea940b10872d5997c8cb8b62830dce08e6020968
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-04-21 09:16:00 -0700

    Added route-based activation and deactivation of the Loader screen

--- ClientApp/src/app/services/loader.service.ts
+++ ClientApp/src/app/services/loader.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { Router, NavigationEnd } from '@angular/router';
 
+export interface LoadingMap {
+    [route: string]: boolean;
+}
 @Injectable()
 export class LoaderService {
 
     loadingStatus: Subject<boolean> = new Subject<boolean>();
-
-    constructor() { }
+    currentRoute: string;
+    loadMap: LoadingMap = {};
+    constructor(private router: Router) {
+        this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .map((event: NavigationEnd) => event.urlAfterRedirects.split('/').join(''))
+            .subscribe((r) => {
+                this.currentRoute = r;
+                const status = !!this.loadMap[r];
+                this.loadingStatus.next(status);
+            });
+    }
 
     startLoading() {
+        this.loadMap[this.currentRoute] = true;
         this.loadingStatus.next(true);
     }
 
-    endLoading() {
-        this.loadingStatus.next(false);
+    endLoading(route: string) {
+        this.loadMap[route] = false;
+        if (this.currentRoute === route) {
+            this.loadingStatus.next(false);
+        }
+        //TODO: Fix this killing an active loader when another returns;
     }
 
     getLoadingStatus(): Subject<boolean> {
