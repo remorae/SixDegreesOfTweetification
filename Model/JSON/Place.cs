dfa011196ba3158d964dcaf2b62b06c87f08cb90
Alex Plagman <alexplagman@gmail.com>
2018-04-26 15:16:11 -0700

    Compare places only by ID

---
+++ Model/JSON/Place.cs
@@ -0,0 +1,15 @@
+﻿namespace SixDegrees.Model.JSON
+{
+    public partial class Place
+    {
+        public override bool Equals(object obj)
+        {
+            var other = obj as Place;
+            if (other == null)
+                return false;
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
