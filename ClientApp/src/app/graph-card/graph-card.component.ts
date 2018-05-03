58b5509f231ff44b2787afd7cf948b1f4c4f857c
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-05-02 20:38:31 -0700

    Add TitleCase Method to Graph Card

--- ClientApp/src/app/graph-card/graph-card.component.ts
+++ ClientApp/src/app/graph-card/graph-card.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    OnChanges,
+    SimpleChanges
+} from '@angular/core';
 import { EndpointService } from '../services/endpoint.service';
 import { Subject } from 'rxjs/Subject';
 import { UserResult } from '../models/UserResult';
@@ -12,22 +18,24 @@ import { Link, Graph } from '../services/graph-data.service';
     styleUrls: ['./graph-card.component.scss']
 })
 export class GraphCardComponent implements OnInit, OnChanges {
-
     @Input() nodeData;
     @Input() whatToWhat;
     @Input() graph;
     cardTitle: string;
     cardBody = [];
     userData: Subject<any> = new Subject<any>();
-    constructor(private endpoint: EndpointService) { }
+    constructor(private endpoint: EndpointService) {}
 
     ngOnInit() {
-        this.userData.pipe(
-            switchMap((data: { id }) => this.endpoint.getUserInfo(data.id)
-                .pipe(
-                    catchError(err => Observable.empty<UserResult>())
+        this.userData
+            .pipe(
+                switchMap((data: { id }) =>
+                    this.endpoint
+                        .getUserInfo(data.id)
+                        .pipe(catchError(err => Observable.empty<UserResult>()))
                 )
-            )).subscribe((result: UserResult) => {
+            )
+            .subscribe((result: UserResult) => {
                 this.nodeData.user = result;
                 this.updateCardContent(this.nodeData);
             });
@@ -52,26 +60,36 @@ export class GraphCardComponent implements OnInit, OnChanges {
                     continue;
                 }
                 if (key && value) {
-                    this.cardBody.push([key.toString(), value.toString()]);
+                    const keyTitle = this.toTitleCase(key.toString());
+                    this.cardBody.push([keyTitle, value.toString()]);
                 }
-
             }
         } else if (data.isUser) {
-
             this.userData.next(data);
-
         } else {
             const connectedWords: string[] = this.graph.links
-                .filter((link) => link.source.id === data.id || link.target.id === data.id)
-                .map((link) => (link.source.id === data.id) ? link.target.id : link.source.id);
+                .filter(
+                    link =>
+                        link.source.id === data.id || link.target.id === data.id
+                )
+                .map(
+                    link =>
+                        link.source.id === data.id
+                            ? link.target.id
+                            : link.source.id
+                );
             this.cardTitle = '#' + data.id;
             this.cardBody.push('Connected To:');
-            connectedWords.forEach((hash) => {
+            connectedWords.forEach(hash => {
                 this.cardBody.push('#' + hash);
             });
         }
+    }
 
-
+    toTitleCase(str) {
+        return str
+            .replace(/([A-Z])/g, match => ` ${match}`)
+            .replace(/^./, match => match.toUpperCase());
     }
 
     showTimeData(metadata) {
@@ -84,5 +102,4 @@ export class GraphCardComponent implements OnInit, OnChanges {
         this.cardBody.push(['Minutes', minutes]);
         this.cardBody.push(['Seconds', seconds]);
     }
-
 }
