7788c839ec33b46f4b2a43e92fdb18c026251857
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-05-04 14:55:06 -0700

    Add Popup for when a feature finishes loading

--- ClientApp/src/app/services/alert.service.ts
+++ ClientApp/src/app/services/alert.service.ts
@@ -5,20 +5,22 @@ export class Message {
     header: string;
     content: string;
     style: string;
+    link?: string;
 
-    constructor(header, content, style?) {
+    constructor(header, content, style?, link?) {
         this.header = header;
         this.content = content;
         this.style = style || 'info';
+        this.link = link;
     }
-
 }
 @Injectable()
 export class AlertService {
     messages: Message[] = [];
-    alerts: BehaviorSubject<Message[]> = new BehaviorSubject<Message[]>(this.messages);
-    constructor() {
-    }
+    alerts: BehaviorSubject<Message[]> = new BehaviorSubject<Message[]>(
+        this.messages
+    );
+    constructor() {}
 
     getAlerts(): BehaviorSubject<Message[]> {
         return this.alerts;
@@ -26,7 +28,15 @@ export class AlertService {
 
     addError(message: string) {
         this.messages.push(new Message('Error', message, 'danger'));
-
     }
-
+    addLoadingFinishedMessage(route: string) {
+        this.messages.push(
+            new Message(
+                'Finished!',
+                route + ' has finished loading!',
+                'info',
+                '/' + route
+            )
+        );
+    }
 }
--- ClientApp/src/app/services/loader.service.ts
+++ ClientApp/src/app/services/loader.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Router, NavigationEnd } from '@angular/router';
+import { AlertService } from './alert.service';
 
 export interface LoadingMap {
     [route: string]: boolean;
 }
 @Injectable()
 export class LoaderService {
-
     loadingStatus: Subject<boolean> = new Subject<boolean>();
     currentRoute: string;
     loadMap: LoadingMap = {};
-    constructor(private router: Router) {
+    constructor(private router: Router, private alerts: AlertService) {
         this.router.events
             .filter(event => event instanceof NavigationEnd)
-            .map((event: NavigationEnd) => event.urlAfterRedirects.split('/').join(''))
-            .subscribe((r) => {
+            .map((event: NavigationEnd) =>
+                event.urlAfterRedirects.split('/').join('')
+            )
+            .subscribe(r => {
                 this.currentRoute = r;
                 const status = !!this.loadMap[r];
                 this.loadingStatus.next(status);
@@ -35,12 +37,11 @@ export class LoaderService {
             const cue = new Audio('../../assets/SoundEffects/LoadingCue.m4a');
             cue.load();
             cue.play();
+            this.alerts.addLoadingFinishedMessage(route);
         }
-
     }
 
     getLoadingStatus(): Subject<boolean> {
         return this.loadingStatus;
     }
-
 }
