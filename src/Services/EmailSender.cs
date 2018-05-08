0f0c5c52423860e7a49f0ed68ede4c58463142da
Mori <me@morine.net>
2018-05-07 19:29:55 -0700

    Services documentation

--- src/Services/EmailSender.cs
+++ src/Services/EmailSender.cs
@@ -4,7 +4,17 @@ namespace SixDegrees.Services
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        /// <summary>
+        /// Generates a task to send an email with the given subject and message to the given address.
+        /// </summary>
+        /// <remarks>
+        /// NOT IMPLEMENTED.
+        /// </remarks>
+        /// <param name="address">Where to send the email.</param>
+        /// <param name="subject">The email's subject line.</param>
+        /// <param name="message">The content of the email.</param>
+        /// <returns>A task to send the email.</returns>
+        public Task SendEmailAsync(string address, string subject, string message)
         {
             return Task.CompletedTask;
         }
--- src/Services/IEmailSender.cs
+++ src/Services/IEmailSender.cs
@@ -2,8 +2,11 @@
 
 namespace SixDegrees.Services
 {
+    /// <summary>
+    /// Interface for any class which can send emails.
+    /// </summary>
     public interface IEmailSender
     {
-        Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailAsync(string address, string subject, string message);
     }
 }
