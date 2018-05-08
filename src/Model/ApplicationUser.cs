130a8b5bae2cb19104c288c02d975fa4696cda72
Alex Plagman <alexplagman@gmail.com>
2018-05-07 18:24:12 -0700

    Setup DocFX

---
+++ src/Model/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SixDegrees.Model
+{
+    /// <summary>
+    /// Represents an authenticated user of the Six Degrees app.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        // Placeholder for potential custom user logic
+    }
+}
