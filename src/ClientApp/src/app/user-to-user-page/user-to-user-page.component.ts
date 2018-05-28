5bd25ee175ec224b79afcc8870e64090079ac9e2
Nunno Yobizness <kyle.hermens@eagles.ewu.edu>
2018-05-27 18:13:47 -0700

    Docs for section-tile, single-input, tile-column, user2user, wordcloud

--- src/ClientApp/src/app/user-to-user-page/user-to-user-page.component.ts
+++ src/ClientApp/src/app/user-to-user-page/user-to-user-page.component.ts
@@ -7,7 +7,10 @@ import {
 } from '../models/UserResult';
 import { GraphDataService, Graph } from '../services/graph-data.service';
 import { InputCacheService } from '../services/input-cache.service';
-
+/**
+ * @example Houses a dual input component, and requests data using the input user handles,
+ *          which is then passed into a graph-visualizer component.
+ */
 @Component({
     selector: 'app-user-to-user-page',
     templateUrl: './user-to-user-page.component.html',
@@ -24,6 +27,13 @@ export class UserToUserPageComponent implements OnInit {
         private inputCache: InputCacheService
     ) {}
 
+    /**
+     * @example When a graph is loaded from the GraphDataService, the graph-visualizer will only
+     *          be immediately shown if the user has not navigated away from the page.
+     *
+     *          If there was navigation away from the page before the latest graph loaded,
+     *          the previously searched terms are pulled from the InputCacheService.
+     */
     ngOnInit() {
         this.graphData.getLatestUserData().subscribe((g: Graph) => {
             this.userRelationshipGraph = g;
@@ -38,7 +48,13 @@ export class UserToUserPageComponent implements OnInit {
 
         this.freshNavigation = true;
     }
-
+    /**
+     *  @example Clears the current graph locally and in the GraphDataService,
+     *           queries for a new graph with the latest pair of user handles,
+     *           and caches said pair of user handles.
+     *
+     * @param input The pair of user handles input by the user in the dual-input component.
+     */
     onUserSubmit(input: UserInput) {
         const [start, end] = input.inputs;
         this.userRelationshipGraph = undefined;
@@ -48,11 +64,15 @@ export class UserToUserPageComponent implements OnInit {
         this.latestSearchEnd = end;
         this.inputCache.cachePreviousUsers(start, end);
     }
-
+    /**
+     * @example Displays the graph-visualizer modal window.
+     */
     showModal() {
         this.modalActive = true;
     }
-
+    /**
+     *  @example Conditionally hides or displays the graph-visualizer modal.
+     */
     changeModal(value) {
         this.modalActive = value;
     }
